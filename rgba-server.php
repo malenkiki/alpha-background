a3422202b7d4616601e421c2eba6b161755727b8
malenkiki <petit.michel@gmail.com>
2013-10-31 22:00:27 +0100

    Server script writen

--- rgba-server.php
+++ rgba-server.php
@@ -23,3 +23,53 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 
+namespace Malenki;
+
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ALPHA_PATH', __DIR__ . DS . 'src'.DS.'Malenki'.DS);
+include(ALPHA_PATH . 'AlphaBackground.php');
+
+if(isset($_GET['color']))
+{
+    $str_param = $_GET['color'];
+
+    $rgba = new AlphaBackground();
+
+    // color name
+    if(preg_match('/^[a-zA-Z]+,[0-9]\.[0-9]+$/', $str_param))
+    {
+        list($str_name, $float_alpha) = explode(',', $str_param);
+
+        try
+        {
+            $rgba->name(strtolower($str_name));
+            $rgba->alpha((float) $float_alpha);
+
+            $rgba->display();
+        }
+        catch(\Exception $e)
+        {
+            $rgba->display();
+        }
+    }
+
+    // rgba
+    if(preg_match('/^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3},[0-9]\.[0-9]+$/', $_GET['color']))
+    {
+        $arr = explode(',', $str_param);
+
+        try
+        {
+            $rgba->red((int) $arr[0]);
+            $rgba->green((int) $arr[1]);
+            $rgba->blue((int) $arr[2]);
+            $rgba->alpha((float) $arr[3]);
+            $rgba->display();
+        }
+        catch(\Exception $e)
+        {
+            $rgba->display();
+        }
+    }
+}
